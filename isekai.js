dd93ed879b0914c9cf79bf6af8e720b9e57e6e93
Wesley Tsai <wes.tsai97@gmail.com>
2024-04-15 11:29:43 -0500

    aesthetic update, updates to scoring system, but need to add questions and determine scores

--- isekai.js
+++ isekai.js
@@ -2,16 +2,65 @@ const questions = document.querySelectorAll('.question');
 const loading = document.querySelector('.loading');
 const nextButton = document.getElementById('nextButton');
 const answerInputs = document.querySelectorAll('input[type="radio"]');
-let currentIndex = 0;
-let score = 0;
 
-function updateScore(selectedAnswer) {
-    score += parseInt(selectedAnswer.value);
+let currentIndex = 0;
+let personalityScore = {
+    "Hero": 0,
+    "Villain": 0,
+    "Side Character": 0,
+    "Protagonist": 0,
+    "Comic Relief": 0,
+    "NPC Villager": 0,
+    "The Mentor": 0,
+    "The Anti-Hero": 0,
+    "The Love Interest": 0
+};
+let worldScore = {
+    "Typical Fantasy": 0,
+    "Gaming World": 0,
+    "Feudal Era": 0,
+    "Apocalyptic Future": 0,
+    "Slice of Life": 0
+};
+
+// Scoring functions:
+function updatePersonalityScore(selectedAnswer) {
+    characterType = selectedAnswer.value;
+    switch(characterType) {
+        case "Hero":
+            personalityScore["Hero"] += 3;
+            personalityScore["Protagonist"] += 1;
+            personalityScore["Villain"] += 1;
+    }
+    
 }
+
+function updateWorldScore(selectedAnswer) {
+    worldScore[selectedAnswer.value] += 1;
+}
+
+// Find the maximum value in the score objects
+function calculateScores() {
+    let universe;
+    for (let key in worldScore) {
+        if (worldScore[key] > maxScore) {
+            universe = key;
+        }
+    }
+
+    let character;
+    for (let key in personalityScore) {
+        if (personalityScore[key] > maxScore) {
+            character = key;
+        }
+    }
+    return [characeter, universe]
+}
+
 
 function updateButton() {
     if (currentIndex === questions.length - 1) {
-        nextButton.textContent = 'See my Results!';
+        nextButton.textContent = 'Load my World...';
         nextButton.removeEventListener('click', goToNextQuestion);
         nextButton.addEventListener('click', submitQuiz);
     } else {
@@ -27,13 +76,20 @@ function goToNextQuestion() {
         alert('Please select an answer.');
         return; // Exit the function if no answer is selected
     }
+
     if (currentIndex === 0) {
         questions[currentIndex].classList.remove('active');
         currentIndex++;
         questions[currentIndex].classList.add('active');
     }
+    else if (currentIndex <= 7) {
+        updatePersonalityScore(selectedAnswer);
+        questions[currentIndex].classList.remove('active');
+        currentIndex++;
+        questions[currentIndex].classList.add('active');
+    }
     else {
-        updateScore(selectedAnswer);
+        updateWorldScore(selectedAnswer);
         questions[currentIndex].classList.remove('active');
         currentIndex++;
         questions[currentIndex].classList.add('active');
@@ -45,10 +101,10 @@ function goToNextQuestion() {
 function submitQuiz() {
     questions[currentIndex].classList.remove('active');
     loading.classList.add('active');
-    console.log('Quiz complete.', score);
+    console.log('Quiz complete.');
 
     // Add submit logic here:
-    
+    const scores = calculateScores();
 
 }
 
